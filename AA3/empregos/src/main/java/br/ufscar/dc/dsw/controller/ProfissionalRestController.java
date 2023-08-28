a987188fd221f1f3179525801040d17900db3e6b
jorgeprj <jlmp2002@gmail.com>
2023-08-28 17:39:10 -0300

    fix

--- AA3/empregos/src/main/java/br/ufscar/dc/dsw/controller/ProfissionalRestController.java
+++ AA3/empregos/src/main/java/br/ufscar/dc/dsw/controller/ProfissionalRestController.java
@@ -83,6 +83,7 @@ public class ProfissionalRestController {
 			if (isJSONValid(json.toString())) {
 				Profissional profissional = new Profissional(); 
 				parse(profissional, json);
+				profissional.setRole("PROFISSIONAL");
 				service.salvar(profissional);
 				return ResponseEntity.ok(profissional);
 			} else {
